67273a781736d087e236ddc1e0deb337426abca4
Stephen Li <stephen.li@D2L.com>
2016-05-10 16:06:37 -0400

    HMR setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const validate = require('webpack-validator');
 
+const devServer = require('./webpack/dev_server');
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
@@ -29,7 +31,14 @@ switch (process.env.npm_lifecycle_event) {
     config = merge(common, {});
     break;
   default:
-    config = merge(common, {});
+    config = merge(
+      common,
+      devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
 }
 
 module.exports = validate(config);
